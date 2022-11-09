570eac172832f0059821aa7397540a7b076e0968
TomaszPosluszny <tomkposluszny@gmail.com>
2022-11-09 14:22:09 +0100

    Adding a function to the clear button

--- js/script.js
+++ js/script.js
@@ -4,4 +4,12 @@ const pass2 = document.querySelector('#password2');
 const email = document.querySelector('#email');
 const sendBtn = document.querySelector('.send');
 const clearBtn = document.querySelector('.clear');
-const popup = document.querySelector('.popup')
+const popup = document.querySelector('.popup');
+
+clearBtn.addEventListener('click', (e) => {
+	e.preventDefault();
+
+	[username, pass, pass2, email].forEach((el) => {
+		el.value = '';
+	});
+});
