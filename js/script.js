c73929fd183b659588911e51aa65d1649f9a20e5
TomaszPosluszny <tomkposluszny@gmail.com>
2022-11-21 18:45:06 +0100

    adding recent functionalities

--- js/script.js
+++ js/script.js
@@ -40,6 +40,31 @@ const checkPassword = (pass1, pass2) => {
         showError(pass2, 'Hasła do siebie nie pasują.')
     }
 }
+const checkEmail = email => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (re.test(email.value)) {
+        clearError(email)
+    } else {
+        showError(email, 'E-mail jest niepoprawny')
+    }
+}
+
+const checkErrors = () => {
+
+    const allInputs = document.querySelectorAll('.form-box');
+    let errorCount = 0;
+
+    allInputs.forEach(el => {
+        if (el.classList.contains('error')) {
+            errorCount++
+        }
+    })
+
+    if (errorCount === 0) {
+        popup.classList.add('show-popup')
+    }
+}
 sendBtn.addEventListener('click', (e) => {
 	e.preventDefault();
 
@@ -47,6 +72,8 @@ sendBtn.addEventListener('click', (e) => {
 	checkLength(username, 3);
 	checkLength(pass, 8);
 	checkPassword(pass, pass2)
+	checkEmail(email);
+    checkErrors()
 });
 
 clearBtn.addEventListener('click', (e) => {
